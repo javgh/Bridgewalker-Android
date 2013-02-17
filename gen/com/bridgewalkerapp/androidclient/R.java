e5e6c548e976b9223aa88b080cf94c93aa32b923
Jan Vornberger <jan@uos.de>
2013-02-17 22:43:49 +0100

    Integrated new logo

--- gen/com/bridgewalkerapp/androidclient/R.java
+++ gen/com/bridgewalkerapp/androidclient/R.java
@@ -918,8 +918,13 @@ containing a value of this type.
         public static final int abs__textfield_searchview_right_holo_dark=0x7f02006d;
         public static final int abs__textfield_searchview_right_holo_light=0x7f02006e;
         public static final int ic_action_search=0x7f02006f;
-        public static final int ic_launcher=0x7f020070;
-        public static final int ic_menu_share=0x7f020071;
+        public static final int ic_menu_share=0x7f020070;
+        public static final int logo_black=0x7f020071;
+        public static final int logo_black_with_bg=0x7f020072;
+        public static final int logo_large_black=0x7f020073;
+        public static final int logo_large_black_with_bg=0x7f020074;
+        public static final int logo_large_white=0x7f020075;
+        public static final int logo_white=0x7f020076;
     }
     public static final class id {
         public static final int abs__action_bar=0x7f040022;
